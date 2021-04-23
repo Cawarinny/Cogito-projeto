e8da689111b6f187c4c940767828cde4cb70e6ff
Cawarinny <71047714+Cawarinny@users.noreply.github.com>
2021-04-23 14:18:24 -0300

    Update index.py
    Fufc

--- index.py
+++ index.py
@@ -8,4 +8,6 @@ def compassApi():
     dadosGraphic = cp.compass()
     return dadosGraphic
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
