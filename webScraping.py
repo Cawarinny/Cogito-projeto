365a7ce4a103318f9ccbc9ef8467e732ca6f60a4
Cawarinny <71047714+Cawarinny@users.noreply.github.com>
2021-04-23 11:32:24 -0300

    Update webScraping.py

--- webScraping.py
+++ webScraping.py
@@ -11,12 +11,13 @@ def webScraping():
     sleep = 20
 
     # Iniciando Browser
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    co = webdriver.ChromeOptions()
+    co.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    co.add_argument("--headless")
+    co.add_argument("--no-sandbox")
+    co.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options = co)
     
     try:
         # Url usados no processo
